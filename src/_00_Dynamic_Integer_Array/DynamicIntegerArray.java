a4c298a3972b68b033043af836f9a70728321aad
missl <missl@LAPTOP-UNQ0FKIG>
2020-05-07 16:39:13 -0700

    finished DynamicIntegerArray, almost finished IntoToArrayLists

--- src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
+++ src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
@@ -2,33 +2,38 @@ package _00_Dynamic_Integer_Array;
 
 public class DynamicIntegerArray {
 	//1. Create a private int array. Don't initialize it.
-
+	int array[];
 	
 	public DynamicIntegerArray() {
 		//2. Initialize the int array to have 0 elements. 
 		//   This will prevent a null pointer exception
-	
+	array = new int[0];
 	}
 	
 	//3. Complete the steps in the add method
 	public void add(int v) {
 		//A. create and initialize a new int array to be one 
 		//   element longer than the member array
-		
+		int temp[] = new int[array.length + 1];
 		//B. set the last element of your new array to 
 		//   the value passed into the method
+		temp[temp.length-1] = v;
 		
+	
 		//C. iterate through the member array and 
 		//   copy every element from the member array 
 		//   to the new array
-		
+		for(int i =0; i < array.length; i++) {
+			temp[i] = array[i];
+		}
 		//D. set the member array equal to the new array.
+		array = temp;
 	}
 	
 	//4. Complete the steps in the get method
 	public int get(int location) {
 		//A. Return the value of the memeber array at the location passed in
-		return 0;
+		return array[location];
 	}
 	
 	//5. Run the DynamicArrayTest to see if you are correct so far.
@@ -38,15 +43,26 @@ public class DynamicIntegerArray {
 	public void set(int v, int location) {
 		//A. set the variable at the location passed in to the method
 		//   to the new value v
+		array[location] = v;
 	}
 	
 	//7. Complete the steps in the insert method
 	public void insert(int v, int location) {
 		//A. create and initialize a new int array to be one 
 		//   element longer than the member array
-		
+		int temp[] = new int[array.length + 1];
 		//B. Make a for loop that iterates through the new array
-
+		for(int i = 0; i < temp.length; i++) {
+			if(i < location) {
+				temp[i] = array[i];
+			}
+			else if(i > location) {
+				temp[i] = array[i - 1];
+			}
+			else {
+				temp[i] = v;
+			}
+		}
 			//C. if i is less than location:
 		    //		set the element at i of the new array to the element at i of the member array
 			
@@ -54,7 +70,7 @@ public class DynamicIntegerArray {
 		    //		set the element at i of the new array to the i - 1 element of the member array
 			
 			//E. else, set the element at i of the new array to the value v
-		
+		array = temp;
 		
 		//F. set the member array equal to the new array
 	
@@ -65,9 +81,19 @@ public class DynamicIntegerArray {
 	//9. Complete the steps in the remove method
 	public void remove(int location) {
 		//A. create a new array that is one element smaller than the member array
-		
+		int temp[] = new int[array.length - 1];
 		//B. make a for loop to iterate through the member array
-		
+		for(int i = 0; i < array.length; i++) {
+			if (i < location) {
+				temp[i] = array[i];
+			}
+			else if(i > location) {
+				temp[i - 1] = array[i];
+			}
+			else {
+				continue;
+			}
+		}
 			//C. if i  is less than location
 			//		set the element at i of the new array to the element at i of the member array
 			
@@ -77,18 +103,21 @@ public class DynamicIntegerArray {
 			//E. else, continue;
 			
 		//F. set the member array equal to the new array
+		array = temp;
 	}
 	
 	//10. Run the tests again the see if you are correct so far
 	
 	//11. Complete the size method so that it returns the length of the member array.
 	public int size() {
-		return 0;
+		return array.length;
 	}
 	
 	//12. Complete the clear array so that it sets the member array 
 	//    equal to a new integer array of size 0
 	public void clear() {
+		int temp[] = new int[0];
+		array = temp;
 	}
 	
 	//13. Run the test again to see if you are finished.
--- src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
+++ src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
@@ -6,18 +6,41 @@ public class _01_IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-
+		ArrayList<String> array = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		array.add("Pineapple on pizza is bad");
+		array.add("Pepper has pp in it");
+		array.add("This is the third string");
+		array.add("all music genres are valid exept for country");
+		array.add("Chiaki is best girl");
 		//3. Print all the Strings using a standard for-loop
-		
+		System.out.println("Step 3");
+		for(int i = 0; i < array.size(); i++) {
+			String s = array.get(i);
+			System.out.println(s);
+		}
 		
 		//4. Print all the Strings using a for-each loop
-		
+		System.out.println("Step 4");
+		for(String i : array) {
+			System.out.println(i);
+		}
 		//5. Print only the even numbered elements in the list.
-		
+		System.out.println("Step 5");
+		for(int i = 2; i < array.size(); i+=2) {
+			String s = array.get(i);
+			System.out.println(s);
+		}
 		//6. Print all the Strings in reverse order.
-		
+		System.out.println("Step 6");
+		for(int i = array.size()-1; i > 0; i--) {
+			String s = array.get(i);
+			System.out.println(s);	
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
+		System.out.println("Step 7");
+		for(int i = 0; i < array.size(); i++) {
+			//if() FINISH IF STATEMENT
+		}
 	}
 }
