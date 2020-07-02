ee6f70e3c17d91438f248b1eb7b0942be89c4ed3
missl <missl@LAPTOP-UNQ0FKIG>
2020-07-02 16:34:36 -0700

    HHHHHH

--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,9 +1,11 @@
 package _03_IntroToStacks;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Stack;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -26,7 +28,12 @@ public class _02_TextUndoRedo implements KeyListener {
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
 	
-	char back = ;
+	char back = undo();
+	Stack<Character> character = new Stack<Character>();
+	
+	static char undo() {
+		return (char) '\b';
+	}
 	public static void main(String[] args) {
 		new _02_TextUndoRedo().setUp();
 	}
@@ -36,16 +43,25 @@ public class _02_TextUndoRedo implements KeyListener {
 		frame.add(panel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(500,500);
+		frame.addKeyListener(this);
 		frame.setVisible(true);
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
+		//PUSH THE CHAR ONTO THE LABEL ALSO WTF WHY ARE U SO LAZY
+		char letter = e.getKeyChar();
+		character.push(letter);
+		String text = label.getText();
+		label.setText(" ");
+		label.setText(label.getText() + character);
+		
 		// TODO Auto-generated method stub
-		if(e.getKeyChar() == a) {
-			
+		if(e.getKeyChar() == back) {
+			label.setText(text);
+			///FIX THIS
 		}
-	}
+	} 
 
 	@Override
 	public void keyPressed(KeyEvent e) {
