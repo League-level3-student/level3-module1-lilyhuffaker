ebe5aaa510d637d21e0f5e51a9d46e6c872a4107
missl <missl@LAPTOP-UNQ0FKIG>
2020-07-16 16:26:58 -0700

    uhhh

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -2,6 +2,7 @@ package _04_HangMan;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.Stack;
 
 import javax.swing.JFrame;
@@ -16,6 +17,9 @@ public class HangMan implements KeyListener{
 	JLabel lives = new JLabel();
 	
 	Stack<String> words = new Stack<String>();
+	ArrayList<Character> FullWord = new ArrayList<Character>(); 
+	ArrayList<Character> Dashes = new ArrayList<Character>(); 
+	
 	int life = 6;
 	
 	public static void main(String[] args) {
@@ -29,6 +33,7 @@ public class HangMan implements KeyListener{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(200,200);
 		frame.addKeyListener(this);
+		//lives.setLocation(100, 150);
 		frame.setVisible(true);
 		
 		String answer = JOptionPane.showInputDialog("How many rounds do you want to play? (1-266)");
@@ -41,12 +46,17 @@ public class HangMan implements KeyListener{
 	}
 	
 	public void play() {
+		life = 6;
+		lives.setText("Lives: "+life);
 		String selectedWord = words.pop();
+		System.out.println(selectedWord);
 		label.setText("");
 		int length = selectedWord.length();
 		for(int i = 0; i < length; i++) {
-			label.setText(label.getText()+" _ ");
-		}//Work on the key pressed methods
+			Dashes.add('_');
+			label.setText(""+Dashes);
+			FullWord.add(selectedWord.charAt(i));
+		}
 	}
 	
 	public void updateLives() {
@@ -56,19 +66,41 @@ public class HangMan implements KeyListener{
 		else {
 			JOptionPane.showMessageDialog(null, "You have lost all your lives!");
 			String yOrn = JOptionPane.showInputDialog("would you like to play again? (y/n)");
-			if(yOrn == "y" || yOrn == "yes" ) {
+			if(yOrn.contains("y")) {
 				setUp();
 			}
 			else{
 				frame.dispose();
 			}
 		}
+		lives.setText("Lives: "+life);
+	}
+	
+	public void Win() {
+		JOptionPane.showMessageDialog(null, "Congrats! You won!");
+			play();
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		if(FullWord.contains(e.getKeyChar())) {
+			//Give them a letter
+			System.out.println("You got a letter!");
+			for(int i = 0; i < FullWord.size(); i++) {
+				if(e.getKeyChar() == FullWord.get(i)) {
+					//Thats the letter
+					Dashes.set(i, e.getKeyChar());
+				}
+			}
+			label.setText(""+Dashes);
+			if(!Dashes.contains('_')) {
+				Win();
+			}
+		}
+		else {
+			updateLives();
+		}
 	}
 
 	@Override
