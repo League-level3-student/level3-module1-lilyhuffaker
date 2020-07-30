c553998491e0c7a2f7a4deb3c34a28fbe3b477dc
missl <missl@LAPTOP-UNQ0FKIG>
2020-07-30 16:20:10 -0700

    nice

--- src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
+++ src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
@@ -1,7 +1,15 @@
 package _06_Intro_To_Hash_Maps;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
-public class _02_LogSearch {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_LogSearch implements ActionListener {
   /* 
 	 * Crate a HashMap of Integers for the keys and Strings for the values.
 	 * Create a GUI with three buttons. 
@@ -29,5 +37,76 @@ public class _02_LogSearch {
 	 * 				is not in the list. 
 	 *
 	 * */
+	HashMap<Integer, String> log = new HashMap<Integer, String>();
 	
+	 JFrame frame = new JFrame();
+	 JPanel panel = new JPanel();
+	 JButton add = new JButton("Add entry");
+	 JButton search = new JButton("Search by ID");
+	 JButton view = new JButton("View list");
+	 JButton remove = new JButton("Remove Entry");
+	 
+	public static void main(String[] args) {
+		new _02_LogSearch().setUp();
+	}
+	
+	public void setUp() {
+		add.addActionListener(this);
+		view.addActionListener(this);
+		search.addActionListener(this);
+		remove.addActionListener(this);
+		
+		panel.add(add);
+		panel.add(search);
+		panel.add(view);
+		panel.add(remove);
+		frame.add(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton buttonClicked = (JButton)e.getSource();
+		
+		if(buttonClicked == add) {
+			String id = JOptionPane.showInputDialog("Please enter an ID number");
+			String name = JOptionPane.showInputDialog("Please enter the name");
+			int ID = Integer.parseInt(id);
+			log.put(ID, name);
+		}
+		
+		if(buttonClicked == search) {
+			String idsearch = JOptionPane.showInputDialog("Please enter an ID number");
+			int IDsearch = Integer.parseInt(idsearch);
+			if(log.containsKey(IDsearch)) {
+				System.out.println(log.get(IDsearch));
+			}
+			else {
+				System.out.println("Invalid ID");
+			}
+		}
+		
+		if(buttonClicked == view) {
+			for(Integer i : log.keySet()) {
+				System.out.println("ID: "+i+" Name: "+log.get(i));
+			}
+			
+		}
+		
+		if(buttonClicked == remove) {
+			String id = JOptionPane.showInputDialog("Please enter an ID number");
+			int ID = Integer.parseInt(id);
+			if(log.containsKey(ID)) {
+				System.out.println("removed "+log.get(ID));
+				log.remove(ID);
+			}
+			else {
+				System.out.println("Invalid ID");
+			}
+		}
+	}
 }
